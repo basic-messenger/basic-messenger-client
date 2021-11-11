673d4769a9d7a999dbf2a71d89990fc81effda79
Heather Bisgaard <vbchomp@gmail.com>
2021-11-10 21:22:15 -0800

    created the js and css files for app, footer and header. wiwill add more tomorrow when I see more of what is happening with the app. send me questions, comments, concerns in slack and I'll get back to you

---
+++ footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './footer.css';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <p>TLMH&copy;2021</p>
+      </footer>
+    )
+  }
+}
+
+export default Footer;
